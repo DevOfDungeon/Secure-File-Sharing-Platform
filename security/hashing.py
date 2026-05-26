4949c46633f0863ea8852c3405324e6578233c4f
DevOfDungeon <262024899+DevOfDungeon@users.noreply.github.com>
2026-05-26 10:04:31 +0000

    added SHA256 file integrity hashing and included it in tests

--- security/hashing.py
+++ security/hashing.py
@@ -1,4 +1,5 @@
 import bcrypt
+import hashlib as hlib
 def hash_password(password):
     # Generate a salt
     salt = bcrypt.gensalt()
@@ -7,16 +8,25 @@ def hash_password(password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     
     return hashed_password
+
 def verify_password(password, hashed_password):
     # Verify the password against the hashed password
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+def sha256(file):
+    with open(file, "rb") as f:
+        file_content = f.read()
+    return hlib.sha256(file_content).hexdigest()
+
 if __name__ == "__main__":
     
     password = "123456"
     hashed = hash_password(password)
     print(f"Hashed password: {hashed}")
     
-    
+    file_hash = sha256("example.txt")
+    print(f"SHA256 hash of file: {file_hash}")
+
     is_valid = verify_password(password, hashed)
     print(f"Password is valid: {is_valid}")
     
